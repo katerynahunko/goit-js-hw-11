554c746c95560c05231dd1ad9a1d5d15c4e54914
your_space <katerinakaterina182@gmail.com>
2023-09-06 03:44:39 +0300

    +

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+import Notiflix from "notiflix";
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+
+
+
+const API_KEY = '39274431-d607d741389e2ed039dd0643f';
+const URL = 'https://pixabay.com/api/';
+
+
+
+
+// key - твій унікальний ключ доступу до API.
+// q - термін для пошуку. Те, що буде вводити користувач.
+// image_type = photo
+// orientation = horizontal
+// safesearch = true
